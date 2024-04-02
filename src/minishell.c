d42c08f1c37f15767696984ec788068d0bc912d9
Philip <macpilot23@outlook.com>
2024-04-02 23:18:10 +0100

    2024.04.02

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 18:49:09 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/02 03:19:11 by Philip           ###   ########.fr       */
+/*   Updated: 2024/04/02 23:15:03 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ typedef struct s_cmd_list t_cmd_list;
 struct s_cmd_list
 {
 	char		**cmd_argv; // {"echo", "-n", "123", NULL}
-	char		**redirect; // {"<infile", "<<delimiter", ">outfile", ">>outfile"}
+	char		**redirects; // {"<infile", "<<delimiter", ">outfile", ">>outfile"}
 	t_cmd_list	*next;
 };
 
@@ -74,9 +74,110 @@ t_cmd_list	*cmd_list_new(void)
 	return((t_cmd_list *)ft_calloc(1, sizeof(t_cmd_list)));
 }
 
+bool	is_redirect(char c)
+{
+	return (c == '<' || c == '>');
+}
+
 bool	is_divider(char c)
 {
-	return (c == '<' || c == '>' || c == '|');
+	return (is_redirect(c) || c == '|');
+}
+
+char	**list_to_string_array(t_list *list)
+{
+	int		list_len;
+	int		i;
+	char	**string_array;
+
+	if (!list)
+	{
+		return (NULL);
+	}
+	list_len = ft_lstsize(list);
+	string_array = (char **)ft_calloc(list_len + 1, sizeof(char *));
+	i = 0;
+	while (i < list_len)
+	{
+		string_array[i] = (char *)list->content;
+		list = list->next;
+		i++;
+	}
+	return (string_array);
+}
+
+void	free_all_nodes_leave_content(t_list **head)
+{
+	t_list	*node;
+	t_list	*next_node;
+
+	if (!head || !(*head))
+		return ;
+	node = *head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node);
+		node = next_node;
+	}
+	*head = NULL;
+}
+
+bool	is_quotation_mark(char c)
+{
+	return (c == '\'' || c == '\"');
+}
+
+char	*complete_quoted_string(const char *line, size_t *i)
+{
+	char	*end_of_argument;
+	char	*quoted_string;
+	size_t	i_copy;
+
+	i_copy = *i;
+	end_of_argument = ft_strchr(&line[i_copy + 1], line[i_copy]);
+	while (end_of_argument
+		&& is_quotation_mark(*(end_of_argument + 1)))
+	{
+		end_of_argument = ft_strchr(end_of_argument + 1, *end_of_argument);
+	}
+	if (end_of_argument)
+	{
+		// size_t n = end_of_argument - &line[i_copy]; /* Test */
+		quoted_string = ft_strndup(&line[i_copy], end_of_argument - &line[i_copy] + 1);
+		i_copy = end_of_argument - line + 1;
+	}
+	else
+	{
+		quoted_string = ft_strdup(&line[i_copy]);
+		while (line[i_copy])
+			i_copy++;
+	}
+	*i = i_copy;
+	return (quoted_string);
+}
+//123"123"123'123'
+//  123"123 123"123
+// "123 123"123'123'
+// "123"123'123
+// Should be used for arguments and redirect filenames/delimters
+char	*get_complete_string(const char *line)
+{
+	// TODO: left here, refer to the part of line 287 and 292
+
+}
+
+void	add_this_cmd_to_list(t_cmd_list	**cmds, t_cmd_list **this_cmd,
+		t_list **this_cmd_argv_list, t_list **this_cmd_redirect_list)
+{
+	printf("Detects pipe |, recording new command\n");
+	(*this_cmd)->cmd_argv = list_to_string_array(*this_cmd_argv_list);
+	(*this_cmd)->redirects = list_to_string_array(*this_cmd_redirect_list);
+	free_all_nodes_leave_content(this_cmd_argv_list);
+	free_all_nodes_leave_content(this_cmd_redirect_list);
+	cmd_list_append(cmds, *this_cmd);
+	*this_cmd = cmd_list_new();
+	printf("Recording new command\n");
 }
 
 // Lexical analysis, sometimes referred to as tokenizer
@@ -104,29 +205,24 @@ t_cmd_list	*analyze_leximes(const char *line)
 	{
 		if (line[i] == '|')         /* Pipe */
 		{
-			// printf("Detects pipe |\n");
+			/*
+			printf("Detects pipe |, recording new command\n");
+			this_cmd->cmd_argv = list_to_string_array(this_cmd_argv_list);
+			this_cmd->redirects = list_to_string_array(this_cmd_redirect_list);
+			free_all_nodes_leave_content(&this_cmd_argv_list);
+			free_all_nodes_leave_content(&this_cmd_redirect_list);
 			cmd_list_append(&cmds, this_cmd);
+			this_cmd = cmd_list_new();
+			printf("Recording new command\n");
+			// */
+			// /*
+			add_this_cmd_to_list(&cmds, &this_cmd, &this_cmd_argv_list,
+				&this_cmd_redirect_list);
+			// */
 			i++;
 			while (line[i] && ft_isspace(line[i]))
 				i++;
-			this_cmd = cmd_list_new();
 		}
-		else if (line[i] == '<')
-			if (line[i + 1] == '<') /* Heredoc */
-				;
-			else                    /* Infile redirect */
-				;
-		else if (line[i] == '>')
-			if (line[i + 1] == '>') /* Outfile append */
-				;
-			else                    /* Outfile truncate */
-				;
-		else if (line[i] == '\'')   /* ' single quotes */
-			;
-		else if (line[i] == '\"')   /* " Double quotes */
-			;
-		else if (line[i] == '$')    /* Variable */
-			;
 		else                        /* The command "echo" */
 		{
 			/* Extract command */
@@ -134,27 +230,24 @@ t_cmd_list	*analyze_leximes(const char *line)
 			while (line[i] && line[i] != '|')
 			{
 				/* Append redirect to linked list */
-				if (is_divider(line[i]))
+				if (is_redirect(line[i]))
 				{
 					// printf("Detectes divider %c\n", line[i]);
-					// TODO Left here
-					// <<    END
-					// <  infile
-					// > outfile
-					// >>outfile
-
 					/* Finds out the redirect instruction string length */
 					int	redirect_str_len;
 					char	*redirect_str;
 
 					redirect_str_len = 0;
-					while (is_divider(line[analysis_idx]))
+					/* |-- Count inital <'s and >'s */
+					while (is_redirect(line[analysis_idx])) 
 					{
 						analysis_idx++;
 						redirect_str_len++;
 					}
+					/* |-- Skip spaces between < and filename/delimiter */
 					while (line[analysis_idx] && ft_isspace(line[analysis_idx]))
 						analysis_idx++;
+					/* |-- Count word length */
 					while (line[analysis_idx]
 						&& !ft_isspace(line[analysis_idx])
 						&& !is_divider(line[analysis_idx]))
@@ -169,7 +262,7 @@ t_cmd_list	*analyze_leximes(const char *line)
 
 					redirect_str_idx = 0;
 					analysis_idx = i;
-					while (is_divider(line[analysis_idx]))
+					while (is_redirect(line[analysis_idx]))
 					{
 						redirect_str[redirect_str_idx++] = line[analysis_idx++];
 					}
@@ -181,71 +274,94 @@ t_cmd_list	*analyze_leximes(const char *line)
 					{
 						redirect_str[redirect_str_idx++] = line[analysis_idx++];
 					}
+					/* Add the redirection string to list */
 					ft_lstadd_back(&this_cmd_redirect_list,
 									ft_lstnew((void *)redirect_str));
-					printf("%s\n", (char *)ft_lstlast(this_cmd_redirect_list)->content);
+					printf("\"%s\" added to list\n", (char *)ft_lstlast(this_cmd_redirect_list)->content);
 					i = analysis_idx;
-
-					// TODO Convert list to string array and assign to command
 				}
-
-
-
 				/* Append command name/argument to linked list */
-				while (line[analysis_idx]
-				&& !ft_isspace(line[analysis_idx])
-				&& !is_divider(line[analysis_idx])) // "'$USER'" "\"$USER\""
-					analysis_idx++;
-				ft_lstadd_back(&this_cmd_argv_list,
-						ft_lstnew((void *)ft_strndup(&line[i], analysis_idx - i)));
-				printf("%s\n", (char *)ft_lstlast(this_cmd_argv_list)->content);
+				else
+				{
+					char	*this_argument;
+					if (is_quotation_mark(line[i]))
+					{
+						this_argument = complete_quoted_string(line, &i);
+					}
+					/* Normal arugments */
+					else
+					{
+						while (line[analysis_idx]
+						&& !ft_isspace(line[analysis_idx])
+						&& !is_divider(line[analysis_idx]))
+							analysis_idx++;
+						this_argument = ft_strndup(&line[i], analysis_idx - i);
+						if (is_quotation_mark(line[analysis_idx]))
+						{
+							char	*temp1;
+							char	*temp2;
 
-				/* Skip spaces until next argument or \0 is found */
-				while (line[analysis_idx] && ft_isspace(line[analysis_idx]))
-					analysis_idx++;
-				i = analysis_idx;
+							temp1 = this_argument;
+							temp2 = complete_quoted_string(line, &analysis_idx);
+							this_argument = ft_strjoin(temp1, temp2);
+							free(temp1);
+							free(temp2);
+						}
+						/* Skip spaces until next argument or \0 is found */
+						while (line[analysis_idx] && ft_isspace(line[analysis_idx]))
+							analysis_idx++;
+						i = analysis_idx;
+					}
+					ft_lstadd_back(&this_cmd_argv_list,
+							ft_lstnew((void *)this_argument));
+					printf("\"%s\" added to list\n", (char *)ft_lstlast(this_cmd_argv_list)->content);
+				}
 			}
-
-			/* Converts linked list to argv string array */
-			int		cmd_argc;
-			int		argv_idx;
-			t_list	*node;
-
-			cmd_argc = ft_lstsize(this_cmd_argv_list);
-			this_cmd->cmd_argv = (char **)ft_calloc(cmd_argc + 1, sizeof(char *));
-			argv_idx = 0;
-			while (cmd_argc)
-			{
-				node = this_cmd_argv_list;
-				this_cmd->cmd_argv[argv_idx] = (char *)this_cmd_argv_list->content;
-				this_cmd_argv_list = this_cmd_argv_list->next;
-				free(node);
-				argv_idx++;
-				cmd_argc--;
-			}
-			
 		}
 	}
+	/* Converts linked list to argv string array */
+	this_cmd->cmd_argv = list_to_string_array(this_cmd_argv_list);
+	this_cmd->redirects = list_to_string_array(this_cmd_redirect_list);
 	cmd_list_append(&cmds, this_cmd);
+
+	/* List nodes should be freed, but the content should not be freed */
+	// ft_lstclear(&this_cmd_argv_list, free);
+	// ft_lstclear(&this_cmd_redirect_list, free);
 	
 	// return (cmds);
 
+	int		cmd_argc;
+	int		argv_idx;
+	t_list	*node;
+	int		redirect_count;
+	int		redirect_idx;
 	t_cmd_list	*cmd;
 	t_cmd_list	*next_cmd;
-	int			argv_idx;
+	// int			argv_idx;
+	// int			redirect_idx;
 
 	cmd = cmds;
 	while (cmd)
 	{
 		next_cmd = cmd->next;
 		printf("===Command===\n");
+		printf("---Arguments---\n");
 		argv_idx = 0;
-		while (cmd->cmd_argv[argv_idx])
+		while (cmd->cmd_argv && cmd->cmd_argv[argv_idx])
 		{
-			printf("%s\n", cmd->cmd_argv[argv_idx]);
-			free(cmd->cmd_argv[argv_idx]);
+			printf("%d: %s\n", argv_idx, cmd->cmd_argv[argv_idx]);
+			// free(cmd->cmd_argv[argv_idx]);
 			argv_idx++;
 		}
+		printf("---Redirects---\n");
+		redirect_idx = 0;
+		while (cmd->redirects && cmd->redirects[redirect_idx])
+		{
+			printf("%d: %s\n", redirect_idx, cmd->redirects[redirect_idx]);
+			// free(cmd->redirects[redirect_idx]);
+			redirect_idx++;
+		}
+		free(cmd->redirects);
 		free(cmd->cmd_argv);
 		free(cmd);
 		cmd = next_cmd;
@@ -269,8 +385,8 @@ int	main(void)
 	printf("STDOUT isatty = %d\n", isatty(STDOUT_FILENO));
 	printf("STDERR isatty = %d\n", isatty(STDERR_FILENO));
 
-	add_history("Second to latest command");
-	add_history("Latest command");
+	// add_history("Second to latest command");
+	// add_history("Latest command");
 
 	char	*line;
 
@@ -278,6 +394,12 @@ int	main(void)
 	{
 		line = readline("minishell $ ");
 		printf("((%s))\n", rl_line_buffer);
+		if (ft_strncmp("exit", line, 5) == 0) // Incomplete, exit should return
+		{
+			free(line);
+			exit (0);
+		}
+		add_history(line);
 		analyze_leximes(line);
 		free(line);
 	}
