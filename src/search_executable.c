8547c14bbdd1ab75827e69e632c1736157fa54e9
Philip <macpilot23@outlook.com>
2024-04-07 17:03:58 +0100

    skip built-in functions

--- src/search_executable.c
+++ src/search_executable.c
@@ -6,7 +6,7 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 01:27:01 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/07 02:36:57 by Philip           ###   ########.fr       */
+/*   Updated: 2024/04/07 17:02:53 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,36 @@
 #include "string_array.h"
 #include "libft.h"
 #include "free_and_null.h"
+
+/* 
+|command |options                                |
+| ------ | ------------------------------------- |
+| echo   | -n                                    |
+| cd     | with only a relative or absolute path |
+| pwd    | no options                            |
+| export | no options                            |
+| unset  | no options                            |
+| env    | no options or arguments               |
+| exit   | no options                            |
+*/
+
+/**
+ * @brief Checks if a command is a built-in function
+ * 
+ * @param cmd Command to check
+ * @return `bool`
+ * @note `echo` `cd` `pwd` `export` `unset` `env` `exit`
+ */
+static bool	is_builtin_function(const char *cmd)
+{
+	return (ft_strncmp("cd", cmd, 10) == 0
+			|| ft_strncmp("env", cmd, 10) == 0
+			|| ft_strncmp("pwd", cmd, 10) == 0
+			|| ft_strncmp("echo", cmd, 10) == 0
+			|| ft_strncmp("exit", cmd, 10) == 0
+			|| ft_strncmp("unset", cmd, 10) == 0
+			|| ft_strncmp("export", cmd, 10) == 0);
+}
 
 void	search_executable(t_cmd_list *cmds, t_env_stack *env)
 {
@@ -32,7 +62,8 @@ void	search_executable(t_cmd_list *cmds, t_env_stack *env)
 	cmd = cmds;
 	while (cmd)
 	{
-		if (ft_strchr(cmd->cmd_argv[0], '/'))
+		if (ft_strchr(cmd->cmd_argv[0], '/')
+			|| is_builtin_function(cmd->cmd_argv[0]))
 		{
 			cmd = cmd->next;
 			continue ;
