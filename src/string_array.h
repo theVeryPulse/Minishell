64c218e0efaa1356515d64a15efe56c5eab2f23b
Philip <macpilot23@outlook.com>
2024-04-09 11:43:22 +0100

    norm

--- src/string_array.h
+++ src/string_array.h
@@ -6,8 +6,13 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 01:46:13 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/07 02:49:52 by Philip           ###   ########.fr       */
+/*   Updated: 2024/04/09 11:43:00 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef STRING_ARRAY_H
+# define STRING_ARRAY_H
+
 void	free_string_array_and_null(char ***string_array);
+
+#endif
