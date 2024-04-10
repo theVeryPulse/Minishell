c942ffd8d0dca9a74d1d9df565c37abb1d99eb0e
pejasco <pejasco@gmail.com>
2024-04-10 18:11:37 +0100

    pwd_updated

---
+++ src/built_in/builtin_pwd .c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_pwd .c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 17:57:10 by chuleung          #+#    #+#             */
+/*   Updated: 2024/04/10 18:09:53 by chuleung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+#define PATH_MAX 4096
+
+/**
+ * @brief Prints the current working directory.
+ *
+ * This function retrieves the current working directory and prints it to stdout.
+ *
+ * @return 0 upon successful execution, 1 if an error occurs.
+ * 
+ * @note This function relies on the getcwd() system call.
+ */
+int	builtin_pwd(void)
+{
+	char	cwd[PATH_MAX];
+
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		printf("%s\n", cwd);
+	else
+	{
+		perror("No such file or directory");
+		return (1);
+	}
+	return (0);
+}
