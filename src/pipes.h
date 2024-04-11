f5569e085bdbc516145a3b47cd4075e0a73d99ee
Philip <macpilot23@outlook.com>
2024-04-11 11:10:43 +0100

    comment

--- src/pipes.h
+++ src/pipes.h
@@ -6,10 +6,16 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 01:19:31 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/11 01:20:06 by Philip           ###   ########.fr       */
+/*   Updated: 2024/04/11 11:08:29 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Structure for pipes
+ * 
+ * @note Pipes is an integer array of length (pipe_count * 2) as each pipe has
+ *       two ends.
+ */
 typedef struct s_pipes
 {
 	int	*pipes;
