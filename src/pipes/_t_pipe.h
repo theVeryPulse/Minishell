3ce81f177910f15a422c2fc81558ab4747f2c613
Philip <macpilot23@outlook.com>
2024-04-12 21:11:53 +0100

    pipe module

--- src/pipes/_t_pipe.h
+++ src/pipes/_t_pipe.h
@@ -1,17 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipes.h                                            :+:      :+:    :+:   */
+/*   _t_pipe.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 01:19:31 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/11 11:08:29 by Philip           ###   ########.fr       */
+/*   Created: 2024/04/12 20:53:58 by Philip            #+#    #+#             */
+/*   Updated: 2024/04/12 20:56:14 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef _PIPE_T_H
+# define _PIPE_T_H
+
 /**
- * @brief Structure for pipes
+ * @brief Structure for pipes. `int *pipes` should be a dynamically allocated
+ *        array of integers.
  * 
  * @note Pipes is an integer array of length (pipe_count * 2) as each pipe has
  *       two ends.
@@ -21,3 +25,5 @@ typedef struct s_pipes
 	int	*pipes;
 	int	pipe_count;
 }	t_pipes;
+
+#endif
