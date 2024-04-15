5c24a0c6e521c0c5dee074469b3c44e574a28664
Philip <macpilot23@outlook.com>
2024-04-15 19:59:02 +0100

    unset done
    working version
    Norm
    documentation

--- src/built_in/builtin_unset.c
+++ src/built_in/builtin_unset.c
@@ -3,41 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_unset.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 21:15:26 by chuleung          #+#    #+#             */
-/*   Updated: 2024/04/15 19:29:03 by chuleung         ###   ########.fr       */
+/*   Updated: 2024/04/15 19:56:47 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../environment_variables/env.h"
-#include "../environment_variables/_env.h"
-#include "libft.h"
-#include <stdlib.h>
-#include <unistd.h>
 
+/**
+ * @brief Removes environment variables.
+ * 
+ * @param cmd_argv String array of the arguments, first one being "unset"
+ * @param env A pointer to the pointer to the environment variables.
+ * @return Always returns 0.
+ */
 int	builtin_unset(char **cmd_argv, t_env **env)
 {
 	int	i;
-	int	exit_status;
-	int	nbr_of_items;
 
-	i = 1;
-	exit_status = 0;
-	nbr_of_items = 0;
-	while (cmd_argv[nbr_of_items])
-		(nbr_of_items++);
-	while (i < nbr_of_items)
+	i = 0;
+	while (cmd_argv[i])
 	{
 		env_remove_by_name(env, cmd_argv[i]);
 		i++;
 	}
-	return (exit_status);
+	return (0);
 }
-
-// void builtin_export(t_env *env, char *cmd_argv)
-// {
-//     if (ft_unset(cmd_argv[0], "unset", 6))
-//         exit (1);
-//     env_remove_by_name(&env, cmd_argv[1]);
-// }
