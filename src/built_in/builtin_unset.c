fd9b56713dd8f6ab1e821443ea43a21a43145c35
pejasco <pejasco@gmail.com>
2024-04-15 19:30:26 +0100

    unset done

--- src/built_in/builtin_unset.c
+++ src/built_in/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 21:15:26 by chuleung          #+#    #+#             */
-/*   Updated: 2024/04/15 17:33:51 by chuleung         ###   ########.fr       */
+/*   Updated: 2024/04/15 19:29:03 by chuleung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int		_unset_arg_check(char **cmd_argv, int *exit_status,
-	int *nbr_of_items)
-{
-	if(cmd_argv[1] == NULL)
-	{
-		ft_dprintf(STDERR_FILENO, "minishell: unset: not enough arguments");
-		*exit_status = 1;
-		return (1);
-	}
-	while (cmd_argv[*nbr_of_items])
-		(*nbr_of_items)++;
-	return (0);
-}
-
-void	builtin_unset(char **cmd_argv, t_env **env)
+int	builtin_unset(char **cmd_argv, t_env **env)
 {
 	int	i;
 	int	exit_status;
@@ -39,14 +25,14 @@ void	builtin_unset(char **cmd_argv, t_env **env)
 	i = 1;
 	exit_status = 0;
 	nbr_of_items = 0;
-	if (_unset_arg_check(cmd_argv, &exit_status, &nbr_of_items))
-		exit (exit_status);
-	if (i < nbr_of_items)
+	while (cmd_argv[nbr_of_items])
+		(nbr_of_items++);
+	while (i < nbr_of_items)
 	{
 		env_remove_by_name(env, cmd_argv[i]);
 		i++;
 	}
-	exit (exit_status);
+	return (exit_status);
 }
 
 // void builtin_export(t_env *env, char *cmd_argv)
