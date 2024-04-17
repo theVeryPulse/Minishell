d615755175e4ba8dacf3bdf9ddf0d0041f967fb4
Philip <macpilot23@outlook.com>
2024-04-17 12:37:20 +0100

    norm

--- src/signal_handler/exit_status.h
+++ src/signal_handler/exit_status.h
@@ -6,18 +6,18 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 21:14:36 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/17 11:12:27 by Philip           ###   ########.fr       */
+/*   Updated: 2024/04/17 12:37:09 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXIT_STATUS_H
 # define EXIT_STATUS_H
 
-#define SYNTAX_ERROR_EXIT_STATUS 2
-#define IS_A_DIRECTORY_EXIT_STATUS 126
-#define PERMISSION_DENIED_EXIT_STATUS 126
-#define NO_SUCH_FILE_EXIT_STATUS 127
-#define SIGINT_EXIT_STATUS 130
-#define SIGQUIT_EXIT_STATUS 131
+# define SYNTAX_ERROR_EXIT_STATUS 2
+# define IS_A_DIRECTORY_EXIT_STATUS 126
+# define PERMISSION_DENIED_EXIT_STATUS 126
+# define NO_SUCH_FILE_EXIT_STATUS 127
+# define SIGINT_EXIT_STATUS 130
+# define SIGQUIT_EXIT_STATUS 131
 
 #endif
--- src/signal_handler/signal_handler.h
+++ src/signal_handler/signal_handler.h
@@ -6,22 +6,29 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 21:03:16 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/16 22:35:22 by Philip           ###   ########.fr       */
+/*   Updated: 2024/04/17 12:36:54 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIGNAL_HANDLER_H
 # define SIGNAL_HANDLER_H
 
+/* Input stage */
+
 extern void	minishell_sigint(int signal);
+
+/* Waiting child execution stage */
 
 extern void	waiting_child_sigint(int signal);
 extern void	waiting_child_sigquit(int signal);
 
+/* Child process signal handling */
+
 extern void	child_sigint(int signal);
 extern void	child_sigquit(int signal);
+
+/* Heredoc status */
 
 extern void	heredoc_sigint(int signal);
 
 #endif
- 
