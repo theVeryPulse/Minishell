99b2b24aa638c13f4f408b0c498019a3649e523d
Philip <macpilot23@outlook.com>
2024-04-24 02:17:24 +0100

    enum over macro

--- src/exit_status.h
+++ src/exit_status.h
@@ -6,20 +6,23 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 21:14:36 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/20 20:04:45 by Philip           ###   ########.fr       */
+/*   Updated: 2024/04/24 02:15:21 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXIT_STATUS_H
 # define EXIT_STATUS_H
 
-# define REDIRECT_FILE_PERMISSION_DENIED 1
-# define SYNTAX_ERROR_EXIT_STATUS 2
-# define IS_A_DIRECTORY_EXIT_STATUS 126
-# define PERMISSION_DENIED_EXIT_STATUS 126
-# define COMMAND_NOT_FOUND_EXIT_STATUS 127
-# define NO_SUCH_FILE_EXIT_STATUS 127
-# define SIGINT_EXIT_STATUS 130
-# define SIGQUIT_EXIT_STATUS 131
+typedef enum e_exit_status
+{
+	REDIRECT_FILE_PERMISSION_DENIED = 1,
+	SYNTAX_ERROR_EXIT_STATUS = 2,
+	IS_A_DIRECTORY_EXIT_STATUS = 126,
+	PERMISSION_DENIED_EXIT_STATUS = 126,
+	COMMAND_NOT_FOUND_EXIT_STATUS = 127,
+	NO_SUCH_FILE_EXIT_STATUS = 127,
+	SIGINT_EXIT_STATUS = 130,
+	SIGQUIT_EXIT_STATUS = 131,
+}	t_exit_status;
 
 #endif
