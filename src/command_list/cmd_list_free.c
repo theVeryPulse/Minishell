d6201c1151cc457b78076b0148a914ff63081002
Philip <macpilot23@outlook.com>
2024-04-26 01:04:12 +0100

    unlink heredoc temp

--- src/command_list/cmd_list_free.c
+++ src/command_list/cmd_list_free.c
@@ -6,16 +6,18 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 10:21:52 by Philip            #+#    #+#             */
-/*   Updated: 2024/04/25 23:29:04 by Philip           ###   ########.fr       */
+/*   Updated: 2024/04/26 01:03:46 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "t_cmd_list.h"
 #include "../free/free.h"
 #include <stdlib.h>
+#include <unistd.h> /* unlink */
 
 /**
- * @brief Frees the command list and resets the pointer to NULL.
+ * @brief Frees the command list and resets the pointer to NULL. Also unlinks
+ *        temporary heredoc files if any.
  * 
  * @param list Pointer to the pointer to the command list.
  */
@@ -32,6 +34,7 @@ extern void	cmd_list_free(t_cmd_list **list)
 		next_node = node->next;
 		free_string_array_and_null(&node->argv);
 		free_string_array_and_null(&node->redirects);
+		unlink(node->heredoc);
 		free(node->heredoc);
 		free(node);
 		node = next_node;
